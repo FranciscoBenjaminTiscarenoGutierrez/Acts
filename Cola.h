930c8f643901725bd5600051b40de528fb11f623
FranciscoBenjaminTiscarenoGutierrez <francisco.tiscareno4032@alumnos.udg.mx>
2024-09-19 11:10:03 -0600

    Add files via upload
    Modificacion del codigo de la clase cola.h implementacion de los metodos y definicion de la clase alumno

---
+++ Cola.h
@@ -0,0 +1,119 @@
+#ifndef COLA_H
+#define COLA_H
+#include <iostream>
+#include <string>
+const int TAM=100;
+
+//***************************Definicion de la clase persona*************************
+//**********************************************************************************
+class Alumno {
+private:
+    std::string nombre;
+    std::string carrera;
+    std::string materias;
+    float promedio;
+
+public:
+    Alumno() : nombre(""), carrera(""), materias(""), promedio(0.0) {}
+
+    friend std::istream& operator>>(std::istream& hand, Alumno& alumno) {
+        std::cout << "Nombre: ";
+        std::getline(hand, alumno.nombre);  //Por razones que escapan a mi comprension esto debe permanecer como getline.
+        std::cout << "Carrera: ";  
+        hand >> alumno.carrera;
+        std::cout << "Materias: ";
+        hand >> alumno.materias;
+        std::cout << "Promedio: ";
+        hand >> alumno.promedio;
+        hand.ignore(); 
+        return hand;
+    }
+
+    friend std::ostream& operator<<(std::ostream& show, const Alumno& alumno) {
+        show << "Nombre: " << alumno.nombre << ", Carrera: " << alumno.carrera 
+             << ", Materias: " << alumno.materias << ", Promedio: " << alumno.promedio;
+        return show;
+    }
+};
+
+
+//***************************************************************************************
+
+
+
+//****************************Definicion de la clase Pila****************************
+//***********************************************************************************
+class Cola
+{
+private:
+    Alumno datos[TAM];
+    int ult;
+    bool elimina(int pos);
+    int inserta(Alumno& elem, int pos);
+
+public:
+    Cola():ult(-1){}
+
+    bool vacia()const{return ult < 0;}
+    bool llena()const{return ult >= TAM - 1;}
+
+    friend std::ostream& operator<<(std::ostream & o, Cola& L);
+
+    void enqueue(){
+
+        if (llena()) {
+            std::cout << "La cola esta llena, no se puede agregar mas elementos" << std::endl;
+            return;
+        }
+
+        //std::cout<<"exception 2"<<std::endl; /////////////////////////
+        std::cin.ignore(); 
+        ult++;
+        std::cin >>datos[ult]; 
+        
+    }
+
+    Alumno& dequeue(){
+
+        std::cout << datos[0] << std::endl;
+    for (int i = 0; i < ult; i++) {
+        
+        datos[i] = datos[i + 1];
+    }
+    
+    ult--; 
+    }
+
+    Cola operator+(int i) const {
+
+        Cola nuevaCola = *this; 
+        for(int fi=0;fi<i;fi++){
+            std::cout<<"Creando solicitud"<<std::endl; /////////////////////////
+        
+        nuevaCola.enqueue();   
+        }
+        return nuevaCola;        
+    }
+
+    Cola operator-(int i) const {
+
+        Cola nuevaCola = *this;
+        if (vacia()) {
+            std::cout << "La cola esta vacia, no se puede eliminar ningun elemento" << std::endl;
+            return nuevaCola;
+        }
+ 
+        for(int fi=0;fi<i;fi++){
+            std::cout<<"Creando Constancia"<<std::endl; /////////////////////////
+        
+        nuevaCola.dequeue();   
+        }
+        return nuevaCola;       
+    }
+
+};
+//*************************************************************************************
+
+#endif // COLA_H
+
+
